eb1697ff5d851b94589429ce37fd543d4362eaca
sdiik <ahmadshiddiq0@gmail.com>
2025-01-16 20:44:25 +0700

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by ahmad shiddiq on 16/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
